4ecc38abbe849a4bc626d164ed1106f30d490012
carlosnumber9 <charlie_theboss023@hotmail.com>
2024-08-31 18:03:27 +0200

    feat: Header and title styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,10 @@ function App() {
 
   return (
     <Provider store={store}>
+      <header>
+        <img className='site-logo' alt='App Logo' src='https://s3.eu-central-1.amazonaws.com/napptilus/level-test/imgs/logo-umpa-loompa.png' />
+        <h3> Oompa Loompa's Crew </h3>
+      </header>
       <h1>Find your Oompa Loompa</h1>
       <h3>There are more than 100k</h3>
     <OompasList />
